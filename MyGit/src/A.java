ae3399d3ed8ab6291ef66c74b3d41b0f9f9c9fab
dongukkim-dev <ehddong7201@gmail.com>
2022-06-22 20:54:23 +0900

    Change in devC branching

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,5 +2,9 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
+		System.out.println("Change in devC branch");
+		
+		C c = new C();
+		c.sayC();
 	}
 }
