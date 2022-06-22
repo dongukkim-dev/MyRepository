30461914fb7db4fe0573882b369eeb08943bf171
dongukkim-dev <ehddong7201@gmail.com>
2022-06-22 20:52:37 +0900

    Change in devB branching

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,5 +2,8 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
+		System.out.println("Change in devB branch");
+		B b = new B();
+		b.sayB();
 	}
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -2,5 +2,6 @@
 public class B {
 	public void sayB() {
 		System.out.println("This is B");
+		System.out.println("Change in devB branch");
 	}
 }
